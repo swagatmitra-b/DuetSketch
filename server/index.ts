6e2fe801f600662b18fb4db282bc7029a70006a1
Swagat <swagatmitrabhattacharya18@gmail.com>
2023-12-18 17:13:36 +0530

    added server

---
+++ server/index.ts
@@ -0,0 +1,71 @@
+import express from "express";
+import { createServer } from "http";
+import cors from "cors";
+import { Server } from "socket.io";
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    // origin: "https://duetsketch.netlify.app",
+    origin: "http://localhost:3000",
+    methods: "GET",
+  },
+});
+
+const members: Map<string, string[]> = new Map();
+
+io.on("connection", (socket) => {
+  socket.on("start", (drawingInfo, room, user) => {
+    io.to(room).emit("start-drawing", drawingInfo, user);
+  });
+  socket.on("draw", (drawingInfo, room) => {
+    io.to(room).emit("receive-drawing", drawingInfo);
+  });
+  socket.on("stop", (room, user) => {
+    io.to(room).emit("stop-drawing", user);
+  });
+  socket.on("room", (room, user) => {
+    if (!members.get(room)) {
+      members.set(room, [user]);
+      socket.join(room);
+      io.to(room).emit("join", user, [user]);
+      return;
+    } else {
+      const current = members.get(room) as string[];
+      if (current.includes(user)) return;
+      current.push(user);
+      socket.join(room);
+      io.to(room).emit("join", user, current);
+    }
+  });
+  socket.on("leave", (room, user) => {
+    let current = members.get(room) as string[];
+    current = current.filter((mem) => mem != user);
+    members.set(room, current);
+    socket.leave(room);
+    io.to(room).emit("leave-room", user, current);
+  });
+  socket.on("wipe", (room, user) => {
+    io.to(room).emit("wipe-drawing", user);
+  });
+  socket.on("undo", (room, user) => {
+    io.to(room).emit("undo-drawing", user);
+  });
+  socket.on("redo", (room, user) => {
+    io.to(room).emit("redo-drawing", user);
+  });
+  socket.on("mode", (room, user) => {
+    io.to(room).emit("mode", user);
+  });
+  socket.on("disco", (room, user) => {
+    io.to(room).emit("disco", user)
+  });
+});
+
+server.listen(PORT, () => console.log("server is running on port", PORT));
