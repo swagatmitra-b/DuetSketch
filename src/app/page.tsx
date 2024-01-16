5148c132581049988b25bfc43839f7564188cb05
Swagat <swagatmitrabhattacharya18@gmail.com>
2024-01-16 15:18:01 +0530

    careless credit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,18 @@ export default function Home() {
   return (
     <div className="min-h-screen flex justify-center items-center">
       <Join />
+      <div className="absolute bottom-3 right-3 italic">
+        <p>
+          created by
+          <a
+            href="https://github.com/swagatmitra-b/duetsketch"
+            target="_blank"
+            className=" text-blue-700 mx-1 font-semibold"
+          >
+            swagat
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
